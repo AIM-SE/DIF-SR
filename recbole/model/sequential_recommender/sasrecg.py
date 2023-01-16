57371f26e8c970ea39a474075bee1ffd749b46b1
Ary <mrgao.ary@gmail.com>
2023-01-16 19:25:16 +0800

    up

--- recbole/model/sequential_recommender/sasrecg.py
+++ recbole/model/sequential_recommender/sasrecg.py
@@ -1,119 +1,87 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2022/02/22 19:32
-# @Author  : Peilin Zhou, Yueqi Xie
-# @Email   : zhoupl@pku.edu.cn
-r"""
-SASRecD
+# @Time    : 2020/9/18 11:33
+# @Author  : Hui Wang
+# @Email   : hui.wang@ruc.edu.cn
+
+"""
+SASRec
 ################################################
 
 Reference:
-    Yueqi Xie and Peilin Zhou et al. "Decouple Side Information Fusion for Sequential Recommendation"
-    Submited to SIGIR 2022.
+    Wang-Cheng Kang et al. "Self-Attentive Sequential Recommendation." in ICDM 2018.
+
+Reference:
+    https://github.com/kang205/SASRec
+
 """
 
 import torch
 from torch import nn
 
 from recbole.model.abstract_recommender import SequentialRecommender
-from recbole.model.layers import FeatureSeqEmbLayer,DIFTransformerEncoder
+from recbole.model.layers import TransformerEncoder
 from recbole.model.loss import BPRLoss
-import copy
 
 
-class SASRecR(SequentialRecommender):
-    """
-    DIF-SR moves the side information from the input to the attention layer and decouples the attention calculation of
-    various side information and item representation
+class SASRec(SequentialRecommender):
+    r"""
+    SASRec is the first sequential recommender based on self-attentive mechanism.
+
+    NOTE:
+        In the author's implementation, the Point-Wise Feed-Forward Network (PFFN) is implemented
+        by CNN with 1x1 kernel. In this implementation, we follows the original BERT implementation
+        using Fully Connected Layer to implement the PFFN.
     """
 
     def __init__(self, config, dataset):
-        super(SASRecR, self).__init__(config, dataset)
+        super(SASRec, self).__init__(config, dataset)
 
         # load parameters info
         self.n_layers = config['n_layers']
         self.n_heads = config['n_heads']
         self.hidden_size = config['hidden_size']  # same as embedding_size
         self.inner_size = config['inner_size']  # the dimensionality in feed-forward layer
-        self.attribute_hidden_size = config['attribute_hidden_size']
         self.hidden_dropout_prob = config['hidden_dropout_prob']
         self.attn_dropout_prob = config['attn_dropout_prob']
         self.hidden_act = config['hidden_act']
         self.layer_norm_eps = config['layer_norm_eps']
 
-        self.selected_features = config['selected_features']
-        self.pooling_mode = config['pooling_mode']
-        self.device = config['device']
-        self.num_feature_field = len(config['selected_features'])
-
         self.initializer_range = config['initializer_range']
         self.loss_type = config['loss_type']
-        self.fusion_type = config['fusion_type']
-
-        self.lamdas = config['lamdas']
-        self.attribute_predictor = config['attribute_predictor']
-        self.attribute_reg_index = config['attribute_regi']
         self.attr_lamdas = config['attr_lamdas']
 
         # define layers and loss
         self.item_embedding = nn.Embedding(self.n_items, self.hidden_size, padding_idx=0)
         self.position_embedding = nn.Embedding(self.max_seq_length, self.hidden_size)
-
-        self.feature_embed_layer_list = nn.ModuleList(
-            [copy.deepcopy(FeatureSeqEmbLayer(dataset,self.attribute_hidden_size[_],[self.selected_features[_]],self.pooling_mode,self.device)) for _
-             in range(len(self.selected_features))])
-
-        self.trm_encoder = DIFTransformerEncoder(
+        self.trm_encoder = TransformerEncoder(
             n_layers=self.n_layers,
             n_heads=self.n_heads,
             hidden_size=self.hidden_size,
-            attribute_hidden_size=self.attribute_hidden_size,
-            feat_num=len(self.selected_features),
             inner_size=self.inner_size,
             hidden_dropout_prob=self.hidden_dropout_prob,
             attn_dropout_prob=self.attn_dropout_prob,
             hidden_act=self.hidden_act,
-            layer_norm_eps=self.layer_norm_eps,
-            fusion_type=self.fusion_type,
-            max_len=self.max_seq_length
+            layer_norm_eps=self.layer_norm_eps
         )
-
-        self.n_attributes = {}
-        self.dataset = dataset
-        self.attribute_map = []
-        attribute = self.selected_features[0]
-        attribute_count = len(dataset.field2token_id[attribute])
-        self.n_attributes[attribute] = attribute_count
-        self.item_attribute = dataset.item_feat['categories'][:, self.attribute_reg_index]
-        attribute_count = max(self.item_attribute)
-        self.attr_embedding = nn.Embedding(attribute_count+1, self.hidden_size, padding_idx=0)
-        if self.attribute_predictor == 'MLP':
-            self.ap = nn.Sequential(nn.Linear(in_features=self.hidden_size,
-                                                       out_features=self.hidden_size),
-                                             nn.BatchNorm1d(num_features=self.hidden_size),
-                                             nn.ReLU(),
-                                             # final logits
-                                             nn.Linear(in_features=self.hidden_size,
-                                                       out_features=self.n_attributes)
-                                             )
-        elif self.attribute_predictor == 'linear':
-            self.ap = nn.ModuleList(
-                [copy.deepcopy(nn.Linear(in_features=self.hidden_size, out_features=self.n_attributes[_]))
-                 for _ in self.selected_features])
 
         self.LayerNorm = nn.LayerNorm(self.hidden_size, eps=self.layer_norm_eps)
         self.dropout = nn.Dropout(self.hidden_dropout_prob)
+
+        attribute = self.selected_features[0]
+        attribute_count = len(dataset.field2token_id[attribute])
+        self.item_attribute = dataset.item_feat['categories'][:, self.attribute_reg_index]
+        attribute_count = max(self.item_attribute)
+        self.attr_embedding = nn.Embedding(attribute_count + 1, self.hidden_size, padding_idx=0)
 
         if self.loss_type == 'BPR':
             self.loss_fct = BPRLoss()
         elif self.loss_type == 'CE':
             self.loss_fct = nn.CrossEntropyLoss()
-            self.attribute_loss_fct = nn.BCEWithLogitsLoss(reduction='none')
         else:
             raise NotImplementedError("Make sure 'loss_type' in ['BPR', 'CE']!")
 
         # parameters initialization
         self.apply(self._init_weights)
-        self.other_parameter_name = ['feature_embed_layer_list']
 
     def _init_weights(self, module):
         """ Initialize the weights """
@@ -137,39 +105,28 @@ class SASRecR(SequentialRecommender):
         subsequent_mask = torch.triu(torch.ones(attn_shape), diagonal=1)  # torch.uint8
         subsequent_mask = (subsequent_mask == 0).unsqueeze(1)
         subsequent_mask = subsequent_mask.long().to(item_seq.device)
+
         extended_attention_mask = extended_attention_mask * subsequent_mask
         extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype)  # fp16 compatibility
         extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
         return extended_attention_mask
 
     def forward(self, item_seq, item_seq_len):
-        item_emb = self.item_embedding(item_seq)
-        # position embedding
         position_ids = torch.arange(item_seq.size(1), dtype=torch.long, device=item_seq.device)
         position_ids = position_ids.unsqueeze(0).expand_as(item_seq)
         position_embedding = self.position_embedding(position_ids)
 
-        feature_table = []
-        for feature_embed_layer in self.feature_embed_layer_list:
-            sparse_embedding, dense_embedding = feature_embed_layer(None, item_seq)
-            sparse_embedding = sparse_embedding['item']
-            dense_embedding = dense_embedding['item']
-        # concat the sparse embedding and float embedding
-            if sparse_embedding is not None:
-                feature_table.append(sparse_embedding)
-            if dense_embedding is not None:
-                feature_table.append(dense_embedding)
-
-        feature_emb = feature_table
-        input_emb = item_emb
+        item_emb = self.item_embedding(item_seq)
+        input_emb = item_emb + position_embedding
         input_emb = self.LayerNorm(input_emb)
         input_emb = self.dropout(input_emb)
 
         extended_attention_mask = self.get_attention_mask(item_seq)
-        trm_output = self.trm_encoder(input_emb,feature_emb,position_embedding, extended_attention_mask, output_all_encoded_layers=True)
+
+        trm_output = self.trm_encoder(input_emb, extended_attention_mask, output_all_encoded_layers=True)
         output = trm_output[-1]
-        seq_output = self.gather_indexes(output, item_seq_len - 1)
-        return seq_output
+        output = self.gather_indexes(output, item_seq_len - 1)
+        return output  # [B H]
 
     def calculate_loss(self, interaction):
         item_seq = interaction[self.ITEM_SEQ]
@@ -188,44 +145,21 @@ class SASRecR(SequentialRecommender):
             test_item_emb = self.item_embedding.weight
             logits = torch.matmul(seq_output, test_item_emb.transpose(0, 1))
             loss = self.loss_fct(logits, pos_items)
+
             test_attr_emb = self.attr_embedding.weight
             attr_logits = torch.matmul(test_item_emb, test_attr_emb.transpose(0, 1))
             attr_loss = self.loss_fct(attr_logits, self.item_attribute)
             total_loss = loss + self.attr_lamdas * attr_loss
 
-            # for i, a_predictor in enumerate(self.ap):
-            #     attribute_logits = a_predictor(seq_output)
-            #     attribute_labels = interaction.interaction[self.selected_features[i]]
-            #
-            #     attribute_labels = nn.functional.one_hot(attribute_labels, num_classes=self.n_attributes[
-            #         self.selected_features[i]])
-            #
-            #     if len(attribute_labels.shape) > 2:
-            #         attribute_labels = attribute_labels.sum(dim=1)
-            #     attribute_labels = attribute_labels.float()
-            #     attribute_loss = torch.mean(attribute_loss[:, 1:])
-            #     loss_dic[self.selected_features[i]] = attribute_loss
-            # if self.num_feature_field == 1:
-            #     total_loss = loss + self.lamdas[0] * attribute_loss
-            #     # print('total_loss:{}\titem_loss:{}\tattribute_{}_loss:{}'.format(total_loss, loss,self.selected_features[0],attribute_loss))
-            # else:
-            #     for i,attribute in enumerate(self.selected_features):
-            #         attribute_loss_sum += self.lamdas[i] * loss_dic[attribute]
-            #     total_loss = loss + attribute_loss_sum
-            #     loss_dic['total_loss'] = total_loss
-                # s = ''
-                # for key,value in loss_dic.items():
-                #     s += '{}_{:.4f}\t'.format(key,value.item())
-                # print(s)
             return total_loss
 
     def predict(self, interaction):
         item_seq = interaction[self.ITEM_SEQ]
         item_seq_len = interaction[self.ITEM_SEQ_LEN]
-        seq_output = self.forward(item_seq, item_seq_len)
         test_item = interaction[self.ITEM_ID]
+        seq_output = self.forward(item_seq, item_seq_len)
         test_item_emb = self.item_embedding(test_item)
-        scores = torch.mul(seq_output, test_item_emb).sum(dim=1)
+        scores = torch.mul(seq_output, test_item_emb).sum(dim=1)  # [B]
         return scores
 
     def full_sort_predict(self, interaction):
@@ -233,5 +167,5 @@ class SASRecR(SequentialRecommender):
         item_seq_len = interaction[self.ITEM_SEQ_LEN]
         seq_output = self.forward(item_seq, item_seq_len)
         test_items_emb = self.item_embedding.weight
-        scores = torch.matmul(seq_output, test_items_emb.transpose(0, 1))  # [B, item_num]
+        scores = torch.matmul(seq_output, test_items_emb.transpose(0, 1))  # [B n_items]
         return scores
