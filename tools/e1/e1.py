81dd440f48e6abb2c02f709ad4ecce9315071610
Ary <mrgao.ary@gmail.com>
2023-01-17 13:49:50 +0800

    up

--- tools/e1/e1.py
+++ tools/e1/e1.py
@@ -7,11 +7,10 @@ i = 0
 group = 8 # 4
 print("use start i as ", i)
 grid = {
-    'n_layers': [2],
-    'attr_l1': [0, 0.5, 1, 2],
-    'attr_l2': [0, 0.5, 1, 2],
-    'attr_l3': [0, 0.5, 1, 2],
-    'attr_l4': [0, 0.5, 1, 2],
+    'attr_l1': [0.1, 0.5, 1, 2],
+    'attr_l2': [0.1, 0.5, 1, 2],
+    'attr_l3': [0.1, 0.5, 1, 2],
+    'attr_l4': [0.1, 0.5, 1, 2],
     # 'attr_l5': [0.1, 0.5, 1, 2],
 
     # @todo Dataset params should be the last in this map
